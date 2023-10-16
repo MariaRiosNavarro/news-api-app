7b2dd50526c4fc2ba41352d295b0079105112902
SuperCoder <propscode254@gmail.com>
2023-10-16 10:44:32 +0200

    test4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
 import { api_key } from "../../config";
+
+var url =
+  "https://newsapi.org/v2/everything?" +
+  "q=Samsung&" +
+  "from=2023-01-16&" +
+  "sortBy=popularity&" +
+  "apiKey=97e29d4302af4f009566d77739fd9f0a";
+
+var req = new Request(url);
+
+fetch(req).then(function (response) {
+  console.log(response.json());
+});
